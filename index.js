189f80a85b52e6a718c0b59446fb08303036a678
Jean <mizero2@gmail.com>
2020-11-24 19:18:03 -0700

    week 2 assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+//1. What do each of the following Boolean
+/*true && false 
+console.log(true && false) => true
+true||false 
+console.log(true||false) => false
+false && false
+console.log(false && false) => false
+true && (true||true)
+console.log(true && (true||true)) => true
+false||1 < 5 
+console.log(false||1 < 5) => true
+5 >= 4 && 1 > 3 
+console.log(5 >= 4 && 1 > 3) =>false
+10 < 4 || 1 > 4 
+console.log(10 < 4 || 1 > 4) =>false
+12 >= 2 && 1 < 24 
+console.log(12 >= 2 && 1 < 24) =>true
+'Hello'.charAt(0) === 'h' = false
+console.log('Hello'.charAt(0) === 'h') =>false
+*/
+//2. Boolean Variable
+let isHotOutSide = true;
+console.log(isHotOutSide);
+let isWeekday = true;
+console.log(isWeekday);
+let hasMoneyInPocket = true;
+console.log(hasMoneyInPocket);
